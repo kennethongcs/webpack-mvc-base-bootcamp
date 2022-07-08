4ec5629e1c5893e93f4ab298dd37c2d46ba25b07
kennethongcs <kenneth.ongcs@gmail.com>
2022-07-08 14:55:44 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+import React from 'react';
 import './styles.scss';
+import { createRoot } from 'react-dom/client';
 
 console.log('hello');
+
+// create jsx element
+const myEl = (
+  <div>
+    <h1>Hey you!</h1>
+    <p>
+      Exercitation velit aliquip duis consequat duis qui commodo incididunt
+      magna in.
+    </p>
+  </div>
+);
+
+const rootElement = document.createElement('div');
+document.body.appendChild(rootElement);
+
+const reactComponent = createRoot(rootElement);
+
+reactComponent.render(myEl);
