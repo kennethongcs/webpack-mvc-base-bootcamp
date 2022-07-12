05877a81f9910bd66946251f52bf02d4a22b5c21
kennethongcs <kenneth.ongcs@gmail.com>
2022-07-12 21:30:05 +0800

    init

--- src/index.js
+++ src/index.js
@@ -2,18 +2,8 @@ import './styles.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-function BigNumber({ number }) {
-  return <h1>number: {number}</h1>;
-}
+const rootElement = document.createElement('div');
+document.body.appendChild(rootElement);
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) => (
-  <BigNumber key={number.toString()} number={number} />
-  // <h1 key={number.toString()}>{number}</h1>
-));
-
-const myEl = (
-  <div>
-    <ul>{listItems}</ul>
-  </div>
-);
+const rootComponent = createRoot(rootElement);
+rootComponent.render();
