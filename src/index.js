7a7b3702e2eeb70862d4dd412d5ce23d6978f957
kennethongcs <kenneth.ongcs@gmail.com>
2022-07-19 23:17:43 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 console.log('hello');
 
@@ -8,6 +9,30 @@ const arr = ['house', 'plane', 'items'];
 
 // creating jsx elements
 const loopOfItems = arr.map((items) => <li>{items}</li>);
+
+const Form = () => {
+  const [name, setName] = useState('');
+
+  const handleChange = (evt) => {
+    // Retrieve input field value from JS event object.
+    const inputName = evt.target.value;
+    // Log input field value to verify what we typed.
+    console.log('inputName:', inputName);
+    // Set the value stored in component state to be the new value.
+    setName(inputName);
+  };
+
+  // The value of the input field always matches the value in the state
+  // variable name. When the input value changes, handleChange updates
+  // the value in the state.
+  return (
+    <div>
+      <input value={name} onChange={handleChange} />
+      <div>Name: {name}</div>
+      {/* <div>Name: hi</div> */}
+    </div>
+  );
+};
 
 // create jsx element
 const myEl = (
@@ -17,14 +42,15 @@ const myEl = (
       Exercitation velit aliquip duis consequat duis qui commodo incididunt
       magna in.
     </p>
-    <ul>{loopOfItems}</ul>
+    {/* <ul>{loopOfItems}</ul> */}
+    <Form />
   </div>
 );
 
 const rootElement = document.createElement('div');
 document.body.appendChild(rootElement);
 
-// const reactComponent = createRoot(rootElement);
+const reactComponent = createRoot(rootElement);
 
-// reactComponent.render(myEl);
-render(myEl, rootElement);
+reactComponent.render(myEl);
+// render(myEl, rootElement);
